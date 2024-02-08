0dae00822993fcc356165f5a5ac263221786eea1
Jacob Chaney <jacobchaneyschool@gmail.com>
2024-02-08 00:45:38 -0600

    Updated code with hints whenever you guess a wrong answer.

--- guess_number.py
+++ guess_number.py
@@ -1,14 +1,24 @@
-number = 10
+number = 5
+choice = ""
 
 print("I'm thinking of a number...")
 guess = int(input("What number am I thinking of? "))
 
-while guess != number or "q":
+while True or (choice == "q"):
     if guess == number:
         print("Congratulations! You guessed the right number.")
+        break
+
+    elif guess > number:
+        print("You're guess was too high!")
+
+    elif guess < number:
+        print("You're guess was too low!")
+
     else:
         print(f"Sorry! The number was {number}.")
-        guess = input("Would you like to continue? (Insert 'q' to quit):  ")
-        if guess == "q":
-            break
+    choice = input("Would you like to continue? (Insert 'q' to quit): ")
+    if choice == "q":
+        break
+    guess = int(input("What number am I thinking of? "))
 
