e8d6fb20b77c786a848b8ede74366c4492d0b1c2
Jacob Chaney <jacobchaneyschool@gmail.com>
2024-02-08 00:54:01 -0600

    Updated code with limit to your amount of guesses.

--- guess_number.py
+++ guess_number.py
@@ -1,14 +1,17 @@
 number = 10
-
+guesses = 3
 print("I'm thinking of a number...")
 guess = int(input("What number am I thinking of? "))
 
-while guess != number or "q":
+while True:
     if guess == number:
         print("Congratulations! You guessed the right number.")
     else:
-        print(f"Sorry! The number was {number}.")
-        guess = input("Would you like to continue? (Insert 'q' to quit):  ")
+        guesses -= 1
+        print(f"Sorry! The number was {number}. You have {guesses} guesses left.")
+        if guesses == 0:
+            break
+        choice = input("Would you like to continue? (Insert 'q' to quit):  ")
         if guess == "q":
             break
-
+    guess = int(input("What number am I thinking of? "))
