8072f86513f88f0e458bf7cd73405734ab5ade25
Jacob Chaney <jacobchaneyschool@gmail.com>
2024-02-08 00:36:50 -0600

    Updated number guessing code to include a 'quit' function.

--- guess_number.py
+++ guess_number.py
@@ -3,7 +3,12 @@ number = 10
 print("I'm thinking of a number...")
 guess = int(input("What number am I thinking of? "))
 
-if guess == number:
-   print("Congratulations! You guessed the right number.")
-else:
-   print(f"Sorry! The number was {number}.")
+while guess != number or "q":
+    if guess == number:
+        print("Congratulations! You guessed the right number.")
+    else:
+        print(f"Sorry! The number was {number}.")
+        guess = input("Would you like to continue? (Insert 'q' to quit):  ")
+        if guess == "q":
+            break
+
